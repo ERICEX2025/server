dfaec970a3f8c14c2ee95c606607e9f105658d33
ERICEX2025 <eric_long_him_ko@brown.edu>
2023-10-23 16:51:35 -0400

    refactor: cleaned up all the code

---
+++ front/mock/src/components/REPL.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { REPLInput } from "./REPLInput";
+import { REPLHistory } from "./REPLHistory";
+import { Mode } from "../enums";
+
+//Extra simple TODO: display html table to newest inputted command and flexbox, tab key
+
+export interface HistoryItem{
+  data: string | string[][];
+  mode: Mode;
+  command: string;
+};
+
+/**
+ * top level REPL component that ocntains the REPL Input and History
+ * contains the history state passed into the child componenets
+ * @returns the high level REPL Component
+ */
+export default function REPL() {
+  const [history, setHistory] = useState<HistoryItem[]>([]);
+
+  return (
+    <div className="repl">
+      <REPLHistory history={history}></REPLHistory>
+      <REPLInput
+        history={history}
+        setHistory={setHistory}
+      ></REPLInput>
+    </div>
+  );
+}
